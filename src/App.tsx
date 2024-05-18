7563dccd7e9555a1108da883ae34882a686100bc
bbirch <birchmanb@gmail.com>
2024-05-18 16:15:53 -0500

    added authentication and userpool configuration

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
 
+/* Amplify */
+import { withAuthenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
+
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
 
@@ -50,4 +54,4 @@ const App: React.FC = () => (
   </IonApp>
 );
 
-export default App;
+export default withAuthenticator(App);

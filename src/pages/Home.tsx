cc75cc37b433f9c9d8149ed98a1034aa0c9fa922
bbirch <birchmanb@gmail.com>
2024-05-18 23:50:18 -0500

    Phaser Demo Added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,20 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+import PhaserContainer from '../components/PhaserContainer';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
+
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Mancard Wallet</IonTitle>
         </IonToolbar>
       </IonHeader>
+
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <PhaserContainer />
       </IonContent>
     </IonPage>
   );
